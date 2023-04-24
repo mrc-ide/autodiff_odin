1560f3a4cd8ec26929ce99b18005108567bc68c1
Marc Baguelin <m.baguelin@imperial.ac.uk>
2023-04-24 22:09:16 +0100

    Allow passing of number of data points

--- SIR_stan.R
+++ SIR_stan.R
@@ -2,15 +2,17 @@ m <- rstan::stan_model("models/SIR.stan")
 
 incidence <- read.csv("data/incidence.csv")
 
+n_data <- 100
 stan_fit <- rstan::sampling(m, data = list(
-  T=100,
-  Y=incidence$cases,
+  T=n_data,
+  Y=incidence$cases[seq(n_data)],
   freq=4),
   chains = 4, iter = 2000, thin = 1, cores = 4, control = list(adapt_delta = 0.95, max_treedepth = 15),
   init = list(list(beta=0.25, gamma = 0.1, I0=1),
               list(beta=1, gamma = 1, I0=10),
               list(beta=1, gamma = 1, I0=10),
               list(beta=1, gamma = 1, I0=10)))
-multistrain_fit <- rstan::extract(stan_fit)
+SIR_fit <- rstan::extract(stan_fit)
 
-rstan::grad_log_prob(stan_fit, c(0.25, 0.1, 1))
+rstan::grad_log_prob(stan_fit,
+                     rstan::unconstrain_pars(stan_fit,list(beta=0.25, gamma = 0.1, I0=1)))
