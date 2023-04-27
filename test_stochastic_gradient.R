41512035891e1b08d2cfcac8c5124e455cfb0d22
Marc Baguelin <m.baguelin@imperial.ac.uk>
2023-04-27 17:57:28 +0100

    Plot multiple simulations

--- test_stochastic_gradient.R
+++ test_stochastic_gradient.R
@@ -4,8 +4,14 @@ data <- mcstate::particle_filter_data(
 
 sir_stoch <- odin.dust::odin_dust("models/sir_stochastic.R")
 
-sir_stoch_model <- sir_stoch$new(pars, 0, 100)
 beta <- 0.25
 gamma <- 0.1
 I0 <- 1
 pars <- list(beta = beta, gamma = gamma, I0 = I0)
+sir_stoch_mod <- sir_stoch$new(pars, 0, 100)
+
+y <- sir_stoch_mod$simulate(c(0, data$time_end))
+i <- sir_stoch_mod$info()$index[["time"]]
+j <- sir_stoch_mod$info()$index[["cases_inc"]]
+matplot(y[i, 1, ], t(y[j, , ]), type = "l", col = "#00000055", lty = 1, las = 1,
+        xlab = "Day", ylab = "Cases")
