9c2cfead6feb611e0472f6c39a0dbf518eb33a31
Marc Baguelin <m.baguelin@imperial.ac.uk>
2023-05-22 22:39:29 +0100

    Copy SIR 4 AD to start implementing stochastic gradient

---
+++ models/sir_stochastic_4_AD.R
@@ -0,0 +1,32 @@
+dt <- 1.0 / freq
+p_IR <- 1 - exp(-(gamma) * dt)
+S0 <- 1000
+freq <- user(4)
+
+N <- S + I + R
+p_inf <- beta * I / N * dt
+p_SI <- 1 - exp(-(p_inf))
+n_SI <- rbinom(S, p_SI)
+n_IR <- rbinom(I, p_IR)
+
+update(time) <- (step + 1) * dt
+update(S) <- S - n_SI
+update(I) <- I + n_SI - n_IR
+update(R) <- R + n_IR
+update(cases_cumul) <- cases_cumul + n_SI
+update(cases_inc) <- if (step %% freq == 0) n_SI else cases_inc + n_SI
+update(draw_n_SI) <- n_SI
+update(draw_n_IR) <- n_IR
+
+initial(time) <- 0
+initial(S) <- S0
+initial(R) <- 0
+initial(I) <- I0
+initial(cases_cumul) <- 0
+initial(cases_inc) <- 0
+initial(draw_n_SI) <- 0
+initial(draw_n_IR) <- 0
+
+beta <- user(0.2)
+gamma <- user(0.1)
+I0 <- user(10)
