d242fc2fec0a8d1cfc52f65c2a86895fc54e5e65
Marc Baguelin <m.baguelin@imperial.ac.uk>
2023-06-14 14:50:45 +0100

    Generate data for compare of logistic function

--- ode_toy_model.R
+++ ode_toy_model.R
@@ -1,13 +1,40 @@
+#remotes::install_github("mrc-ide/odin@mrc-4277", upgrade = FALSE, force = TRUE)
+#remotes::install_github("mrc-ide/odin.dust@mrc-4278", upgrade = FALSE, force = TRUE)
+
 generator <- odin::odin("models/logistic_growth_normal_obs.R")
 
 mod <- generator$new(r=1)
 
-tt <- seq(0, 30, length.out = 101)
+n_obs <- 20
+tt <- seq(0, 25, length.out = 101)
+t_obs <- seq(1, n_obs)
 
-tt <- seq(0, 30, length.out = 101)
 y <- mod$run(tt)
-plot(y, xlab = "Time", ylab = "N", las = 1, main = "")
-
 y2 <- mod$run(tt, 50)
-plot(y, xlab = "Time", ylab = "N", las = 1, main = "")
+plot(y, xlab = "Time", ylab = "N", las = 1, main = "", ylim= c(0,120))
 lines(y2, col = "red")
+
+#default parameters
+N0 <- 1
+K <- 100
+r <- 1
+
+#computes the mid-point time (t0 such that N(t0)=K/2)
+t0 <- log(K/N0-1)/r
+
+#plot the analytical solution of the black model
+N_obs <- K/(1+exp(-r*(t_obs-t0)))
+points(t_obs, N_obs)
+
+#generates noisy observations/data
+sd_noise <- 5
+
+d_df <- data.frame(time = t_obs,
+                   observed = rnorm(N_obs,N_obs, sd_noise))
+
+d <- dust::dust_data(d_df)
+
+points(t_obs, d_df$observed, pch=19, col="grey")
+
+
+
