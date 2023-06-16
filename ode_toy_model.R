88a0a5d5f5ca4caa24f135170491640820a511af
Marc Baguelin <m.baguelin@imperial.ac.uk>
2023-06-16 15:24:26 +0100

    Update to fully support odin_dust compilation of the ode rather than the odin one

--- ode_toy_model.R
+++ ode_toy_model.R
@@ -1,18 +1,17 @@
-remotes::install_github("mrc-ide/odin@mrc-4277", upgrade = FALSE, force = TRUE)
-remotes::install_github("mrc-ide/odin.dust@mrc-4278", upgrade = FALSE, force = TRUE)
+#remotes::install_github("mrc-ide/odin@mrc-4277", upgrade = FALSE, force = TRUE)
+#remotes::install_github("mrc-ide/odin.dust@mrc-4278", upgrade = FALSE, force = TRUE)
+#remotes::install_github("mrc-ide/dust", upgrade = FALSE, force = TRUE)
 
-generator <- odin::odin("models/logistic_growth_normal_obs.R")
+generator <- odin.dust::odin_dust("models/logistic_growth_normal_obs.R")
 
-mod <- generator$new(r=1)
+mod <- generator$new(pars=list(r=1, sd_noise=5), time = 0, n_particles = 1)
 
 n_obs <- 20
 tt <- seq(0, 25, length.out = 101)
 t_obs <- seq(1, n_obs)
 
-y <- mod$run(tt)
-y2 <- mod$run(tt, 50)
-plot(y, xlab = "Time", ylab = "N", las = 1, main = "", ylim= c(0,120))
-lines(y2, col = "red")
+y <- mod$simulate(tt)[,1,]
+plot(tt, y, xlab = "Time", ylab = "N", main = "", type="l", ylim= c(0,120))
 
 #default parameters
 N0 <- 1
@@ -37,17 +36,17 @@ d <- dust::dust_data(d_df)
 points(t_obs, d_df$observed, pch=19, col="grey")
 
 #reversing the ode's
-generator_reverse <- odin::odin("models/reverse_AD_logistic.R")
+generator_reverse <- odin.dust::odin_dust("models/reverse_AD_logistic.R")
 
-reverse_mod <- generator_reverse$new(r=1, N_end=N_obs[20], t_end=20)
+reverse_mod <- generator_reverse$new(pars= list(r=1, N_end=N_obs[20], t_end=20), time=0, n_particles = 1)
 
 tt <- seq(0, 25, length.out = 101)
-reverse_y <- reverse_mod$run(tt)
-lines(reverse_y[,"t_model"], reverse_y[,"N"], lty=3, col="green")
+reverse_y <- reverse_mod$simulate(tt)[,1,]
+lines(reverse_y[2,], reverse_y[1,], lty=3, col="green")
 
-##As we can see this is not very stable!!!
+##As we can see this is slightly unstable!!!
 ##plot the error
-plot(y[1:81,"t"], reverse_y[81:1,"N"]- y[1:81,"N"])
+plot(reverse_y[2,81:1], reverse_y[1,81:1]- y[1:81])
 
 #Let's try to do the same now by refreshing the state at each observation point
 for(i in seq_along(t_obs[-1])){
