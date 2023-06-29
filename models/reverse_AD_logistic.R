121ca06759fb68f76a8f54431c4796fe46121093
Marc Baguelin <m.baguelin@imperial.ac.uk>
2023-06-29 15:41:28 +0100

    Add the adjoints equations to the reverse-time model

--- models/reverse_AD_logistic.R
+++ models/reverse_AD_logistic.R
@@ -1,9 +1,19 @@
 deriv(N) <- r * N * (N / K - 1)
+deriv(adj_N) <- adj_N * r * (1- 2*N/K)
+deriv(adj_K) <- adj_N * r * (N/K)^2
+deriv(adj_r) <- adj_N * N * (1 - N/K)
 deriv(t_model) <- -1
+
 initial(N) <- N_end
 initial(t_model) <- t_end
+initial(adj_N) <- adj_N_end
+initial(adj_K) <- adj_K_end
+initial(adj_r) <- adj_r_end
 
 N_end <- user(1)
 t_end <- user()
+adj_N_end <- user()
+adj_K_end <- user()
+adj_r_end <- user()
 K <- user(100)
 r <- user()
